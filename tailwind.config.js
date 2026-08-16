6be722d312491219eb2c1d8ee37ab19949028153
mdrifat-mia <mdrifatmia399@gmail.com>
2026-08-16 14:26:11 +0600

    Set up Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#020617",
+        section: "#0F172A",
+        primary: "#2563EB",
+        accent: "#60A5FA",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
